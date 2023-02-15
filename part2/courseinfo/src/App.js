91547f6f1ecb867b0c080054211b1a6b74236681
CGfrac <cgfrac@protonmail.com>
2023-02-15 12:22:11 +0100

    Complete exercise 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,34 +1,4 @@
-const Header = ({ title }) => <h2>{title}</h2>
-
-const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
-
-const Content = ({ parts }) => {
-  return (
-    <>
-      {parts.map(part =>
-        <Part key={part.id} part={part} />
-      )}
-    </>
-  )
-}
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((acc, part) => acc + part.exercises, 0)
-
-  return (
-    <p><strong>total of {total} exercises</strong></p>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header title={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
-  )
-}
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
