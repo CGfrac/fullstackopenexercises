36319fdd86716ac93070556f90b2203cab6a7134
CGfrac <cgfrac@protonmail.com>
2023-02-15 12:08:38 +0100

    Complete exercise 2.4

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,4 +1,4 @@
-const Header = ({ title }) => <h1>{title}</h1>
+const Header = ({ title }) => <h2>{title}</h2>
 
 const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
 
@@ -16,7 +16,7 @@ const Total = ({ parts }) => {
   const total = parts.reduce((acc, part) => acc + part.exercises, 0)
 
   return (
-    <p>Number of exercises {total}</p>
+    <p><strong>total of {total} exercises</strong></p>
   )
 }
 
@@ -31,30 +31,57 @@ const Course = ({ course }) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
-    <Course course={course} />
+    <>
+      {courses.map(course => 
+        <Course key={course.id} course={course} />
+      )}
+    </>
   )
 }
 
